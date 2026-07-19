6b2b173f61e4aef82aebf42dd676a0a6e4457eb9
visiting555 <mirachakantekin@gmail.com>
2026-07-19 21:21:10 +0300

    Modify height offset for character alignment
    Adjust height offset to align body/leg joint.

--- cheat.lua
+++ cheat.lua
@@ -1304,7 +1304,7 @@ CreateToggle("Ammo", function(enabled)
             -- HESAPLAMA: Gövde-Bacak birleşimi hedefli
             local frontOffset = targetHead.CFrame.LookVector * 0.7 
             -- HRP merkezini aşağı çekiyoruz ki gövde/bacak birleşimi hizalansın
-            local heightOffset = Vector3.new(0, -0, 0)
+            local heightOffset = Vector3.new(0, -0.5, 0)
             local targetPosition = targetHead.Position + frontOffset + heightOffset
 
             -- Karakteri hedefe döndür ve 180 derece ile yüzünü çevir
